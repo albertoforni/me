bccf55322370d8716804b4bbb94b64e5322f2be6
Alberto Forni <me@albertoforni.com>
2016-11-03 21:30:56 +0000

    Remove state from testing
    The state is private to the component and I want to test the interactions that change the state

--- src/features/home/App.test.jsx
+++ src/features/home/App.test.jsx
@@ -4,45 +4,32 @@ import App from './App';
 import Title from './Title';
 import Nav from './Nav';
 import Section from './Section';
+import { navItems } from './SectionContents';
 
 it('renders a title', () => {
   const wrapper = shallow(<App />);
   expect(wrapper.contains(<Title />)).toEqual(true);
 });
 
-it('renders a navigation', () => {
+it('renders a navigation with the selected item -1', () => {
   const wrapper = shallow(<App />);
   expect(wrapper.find(Nav).length).toEqual(1);
 
-  const selectedItem = wrapper.state('activeSection');
-
   const navProps = wrapper.find(Nav).props();
-  expect(navProps.navItems.length).toEqual(4);
-  expect(navProps.onNavItemClick).toBeInstanceOf(Function);
-  expect(navProps.selectedItem).toEqual(selectedItem);
-});
-
-it('changes the active section onNavItemClick', () => {
-  const wrapper = shallow(<App />);
-
-  const aNewSectionIndex = 123;
-
-  wrapper.find(Nav).props().onNavItemClick(aNewSectionIndex);
-  expect(wrapper.state('activeSection')).toEqual(aNewSectionIndex);
+  expect(navProps.navItems.length).toEqual(navItems.length);
+  expect(navProps.selectedItem).toEqual(-1);
 });
 
 describe('section', () => {
-  it('does not render a section when activeSection is -1', () => {
+  it('does not render a section by default', () => {
     const wrapper = shallow(<App />);
-    expect(wrapper.state('activeSection')).toEqual(-1);
     expect(wrapper.find(Section).length).toEqual(0);
   });
 
-  it('renders a section when activeSection is not -1', () => {
+  it('renders a section when Nav sets the section to 1', () => {
     const wrapper = shallow(<App />);
-    wrapper.setState({
-      activeSection: 1,
-    });
+    wrapper.find(Nav).props().onNavItemClick(1);
+    wrapper.update();
 
     expect(wrapper.find(Section).length).toEqual(1);
   });
