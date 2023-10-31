cc300e03489e244ca979a400e605cad5741caeb9
Alberto Forni <me@albertoforni.com>
2023-10-31 22:48:38 +0100

    move to old folder

---
+++ .next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/nomo":["static\u002Fchunks\u002Fpages\u002Fnomo.js"],sortedPages:["\u002F","\u002F_app","\u002F_error","\u002Fnomo"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
